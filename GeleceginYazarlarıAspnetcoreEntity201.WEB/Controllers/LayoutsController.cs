f8680be6e1d0cdba638128e45b632dade3688bc7
ugur gündüz <ugurrgunduz01@gmail.com>
2023-08-25 16:19:21 +0300

    Add project files.

---
+++ GeleceginYazarlarıAspnetcoreEntity201.WEB/Controllers/LayoutsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeleceginYazarlarıAspnetcoreEntity201.WEB.Controllers
+{
+    public class LayoutsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GeleceginYazarlarıAspnetcoreEntity201.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeleceginYazarlarıAspnetcoreEntity201.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
